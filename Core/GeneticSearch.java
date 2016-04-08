59b13ab07617bee95c2c1aa28b070ea38b0d8711
James Keating <james.keating@fieldaware.com>
2016-04-08 12:25:03 +0100

    Initial GA Push

--- Core/GeneticSearch.java
+++ Core/GeneticSearch.java
@@ -1,7 +1,39 @@
 package Core;
 
-public class GeneticSearch {
+import Interfaces.SolutionType;
+
+public class GeneticSearch implements SolutionType {
+	
         public GeneticSearch() {
 
+        }
+        
+        public void improveSolution() {
+        	
+        }
+        /**
+         *Inspects current CandidateSolution for given SolutionType
+         * @return current CandidateSolutions energy
+         */
+        public int getBestSolutionEnergy() {
+			return 0;
+        	
+        }
+
+        /**
+         *Inspects current CandidateSolution for given SolutionType
+         * @return current CandidateSolutions fitness
+         */
+        public int getBestSolutionFitness() {
+			return 0;
+        	
+        }
+
+        /**
+         *@return running time to get best CandidateSolution
+         */
+        public int getTotalRunningTime() {
+			return 0;
+        	
         }
 }
