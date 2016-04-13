1c6b49b38bd73f32b027ad81d388336460921f6b
Gary Mac Elhinney <Gary.mac-elhinney@ucdconnect.ie>
2016-04-13 21:05:30 +0100

    Updated GUI and Core

--- Core/Core.java
+++ Core/Core.java
@@ -3,11 +3,23 @@ package Core;
 import Candidate.CandidateSolution;
 import Data.PreferenceTable;
 
+import javax.swing.*;
+
 public class Core {
-        private CandidateSolution cs;
+        private static final int MAX_ITERATIONS = 1000;
         private PreferenceTable pt;
+        private GeneticSearch gs;
+        private StochasticSearch ss;
+        private MouseListen ml;
+        private GUI gui;
 
         public Core() {
+                gs = new GeneticSearch(pt);
+                ss = new StochasticSearch(pt);
+                gui = new GUI(this);
+                ml = new MouseListen(this);
+                pt = new PreferenceTable("Project allocation.txt");     //Get GUI to pass file name later....maybe....#Not arsed atm
+
                 //Will instantiate a GUI class here
                 /*
                    while GUI not closed {
@@ -31,5 +43,32 @@ public class Core {
                        }
                   }
                  */
+        }
+
+        public void rightButtonClicked(Object source) {
+        }
+
+        public void leftButtonClicked(Object o) {
+                CandidateSolution cs;
+                JButton button =null;
+
+                if(o instanceof JButton) {
+                        button = (JButton)o;
+                        if(button == gui.getExitButton()) {
+                                System.exit(0);
+                        }
+                } else {
+                        String algorithm = gui.getAlgorithm();
+                        if(algorithm.equalsIgnoreCase("Simulated Annealing")) {
+                                cs = ss.generateSolution();
+                        } else {
+                                cs = gs.generateSolution(MAX_ITERATIONS);
+                        }
+                }
+
+        }
+
+        public static void main(String[] args) {
+                new Core();
         }
 }
--- Core/GUI.java
+++ Core/GUI.java
@@ -1,31 +1,94 @@
 package Core;
 
 import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
-public class GUI extends JFrame{
+public class GUI extends JFrame implements ActionListener{
         private JLabel background;
         private JButton exitButton;
         private MouseListen ml;
+        private JTextArea consoleOutput;
+        private String algorithm;
 
         public GUI(Core c) {
-                super("COMP30050 - Software Engineering");
-
+                super("Comp30050 - Software Enginnering (Map Students to projects)");
+                consoleOutput = new JTextArea();
                 background = new JLabel();
                 exitButton = new JButton();
                 ml = new MouseListen(c);
 
                 this.drawBackground();
+                this.addComboBox();
+                this.makeConsoleDisplay();
+
+                this.buildFrame();
+
+                //updateConsoleDisplay("Hi Gary");
         }
 
         private void drawBackground() {
-                background = new JLabel();
                 background.setLocation(0, 0);
                 background.setSize(700, 400);
 
                 exitButton = new JButton("Exit");
-                exitButton.setLocation(620,320);
-                exitButton.setSize(75, 75);
+                exitButton.setLocation(630, 335);
+                exitButton.setSize(55, 25);
                 exitButton.addMouseListener(ml);
                 background.add(exitButton);
+                exitButton.setVisible(true);
+        }
+
+        private void addComboBox() {
+
+                //create ComboBox and set default algorithm to be Simulated Annealing
+                JComboBox algorithmsBox = new JComboBox(new String[]{"Simulated Annealing", "Genetic"});
+                algorithmsBox.setSelectedIndex(0);
+                algorithm = (String) algorithmsBox.getItemAt(0);        //Set default algorithm
+                algorithmsBox.setSize(150,25);
+                algorithmsBox.addActionListener(this);
+                algorithmsBox.setLocation(50,50);
+
+                background.add(algorithmsBox);
+                algorithmsBox.setVisible(true);
+        }
+
+        private void makeConsoleDisplay() {
+                consoleOutput.setLocation(50, 90);
+                consoleOutput.setSize(300, 100);
+                consoleOutput.setEditable(false);
+                background.add(consoleOutput);
+                exitButton.setVisible(true);
+        }
+
+        public void updateConsoleDisplay(String toDisplay) {
+                consoleOutput.append(toDisplay);
+                background.add(consoleOutput);
+                consoleOutput.setVisible(true);
+        }
+
+        public void buildFrame() {
+                this.setSize(700, 400);
+                this.setResizable(false);
+                this.setLocationRelativeTo(null);
+                this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+                this.add(background);
+                this.setVisible(true);
+        }
+
+        public void actionPerformed(ActionEvent e) {
+                JComboBox cb = (JComboBox)e.getSource();
+                String alg = (String)cb.getSelectedItem();
+
+                algorithm = alg;
+        }
+
+        public String getAlgorithm() {
+                return this.algorithm;
+        }
+
+        public JButton getExitButton() {
+                return exitButton;
         }
 }
